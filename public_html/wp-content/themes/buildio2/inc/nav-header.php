442d734fba3f81051676e5fd6637a577db12f9d4
Rohan Kleem <rohankleem@gmail.com>
2024-07-07 11:04:18 +1000

    Toggle mobile menu icon change to svg

--- public_html/wp-content/themes/buildio2/inc/nav-header.php
+++ public_html/wp-content/themes/buildio2/inc/nav-header.php
@@ -7,13 +7,19 @@
 
 
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-default">
-					<i class="bi-list"></i>
-				</span>
-				<span class="navbar-toggler-toggled">
-					<i class="bi-x"></i>
-				</span>
-			</button>
+			<span class="navbar-toggler-default">
+        <!-- Menu Icon SVG -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+    </span>
+    <span class="navbar-toggler-toggled">
+        <!-- Close Icon SVG -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
+    </span>
+        </button>
 
 
 			<div class="collapse navbar-collapse" id="navbarNavDropdown">
